76f11f62814404e87b682d5758d63a789d461a2c
SonaliCharde <chardesonaligithub@gmail.com>
2024-05-31 12:25:43 +0530

    BDD and Non BDD CRUD opereations in RestAssured

---
+++ src/test/java/CRUD/POST/CreateNonBDDStyle.java
@@ -0,0 +1,47 @@
+package CRUD.POST;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+
+public class CreateNonBDDStyle {
+    RequestSpecification requestSpecification;
+    Response response;
+    ValidatableResponse validatableResponse;
+
+    @Test
+    public void CreateBookingNonBDD(){
+        String payload = "{\n" +
+                "    \"firstname\" : \"Jim\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        requestSpecification = RestAssured.given();
+        requestSpecification.given().baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("/booking");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload);
+
+        response = requestSpecification.when().log().all().post();
+
+        validatableResponse = response.then().statusCode(200);
+        String responsebody = response.asString();
+
+        System.out.println(responsebody);
+
+
+
+
+    }
+}
